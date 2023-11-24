838a862a0d0c3d5fbdc889c248a4f6c15fac85ff
Шешулин Кирилл <sheshulin@o-code.ru>
2023-11-24 13:27:56 +0400

    remover

---
+++ remover.py
@@ -0,0 +1,17 @@
+import os
+
+directory_img = 'labeled_data/train/i'
+directory_labels = 'labeled_data/train/l'
+
+remove_filename = 'a4a84dd61b_png.rf.91f9c2102de370d4aeffe343c3285700'
+
+dims = []
+for filename in os.listdir(directory_img):
+    f = os.path.join(directory_img, filename)
+    fl = os.path.join(directory_labels, filename[:-4]+'.txt')
+    if filename[:-4] == remove_filename:
+        os.remove(f)
+        os.remove(fl)
+        print('removed')
+    print(f)
+    print(fl)
